a792a935818f26cfa70a08d596484a41500d653f
RITHIK VASAN <101005626+Rithikvasan@users.noreply.github.com>
2023-05-09 08:35:07 +0530

    Add files via upload

---
+++ barplot.R
@@ -0,0 +1,9 @@
+hist(AirPassengers, 
+     main="Histogram for Air Passengers", 
+     xlab="Passengers", 
+     border="blue", 
+     col="green",
+     xlim=c(100,700),
+     bins=200,
+     las=1, 
+     breaks=5)
